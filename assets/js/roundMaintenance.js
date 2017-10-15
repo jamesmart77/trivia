e1911d7036aec348cbdd2b49662cd6acd77b84ac
James Martineau <James.Martineau@libertymutual.com>
2017-10-15 17:41:22 -0400

    working w/ time remaining, triviaObject complete, dynamic options rendering

--- assets/js/roundMaintenance.js
+++ assets/js/roundMaintenance.js
@@ -1,7 +1,76 @@
-function showQuestion(){
+var intervalID;
+
+function showQuestion() {
+
+    $(".round").html("Round " + trivia.currentRound + 1);//+1 because of 0 index array
+    $(".question").html("Question " + trivia.currentQuestion + 1);
+    $(".time-remaining").html("Time Remaining: " + trivia.timeRemaining);
+
+    //print 4 option buttons for current round question
+    for (var i = 0; i < 4; i++) {
+        let optionElement = $("<button>");
+
+        optionElement.addClass("btn btn-warning option-btn");
+        optionElement.html(trivia.rounds[trivia.currentRound].options[trivia.currentQuestion].choices[i]);
+        $(".question-options").append(optionElement)
+    }
+
+    //start count down on clock
+    intervalID = setInterval(stopWatch, 1000);
+}
+
+function stopWatch() {
+    //decrement by 1
+    let timeLeft = trivia.timeRemaining--;
+
+    //print time remaining
+    if (timeLeft > 0) {
+        $(".time-remaining").html("Time Remaining: " + timeLeft);
+    } else {
+        $(".time-remaining").html("Time is Up!");
+        clearInterval(intervalID);
+    }
 
 }
 
-function nextRound(){
+function nextRound() {
+
+}
+
+//user selects an option
+$(".question-options").on("click", ".option-btn", function () {
+
+    let guess = ($(this).html());
+    let answer = trivia.rounds[trivia.currentRound].answers[trivia.currentQuestion];
+    let result
+    if(guess === answer){
+        trivia.correctAnswers ++;
+        result = "correctly";
+    } else{
+        trivia.incorrectAnswers ++;
+        result = "incorrectly";
+    }
+    clearInterval(intervalID);
+    showResponse(result, answer);
+});
+
+function showResponse(result, answer){
+
+    if(result === "correctly"){
+        $(".answer-response").html("You answered <b>" + result + "</b>!")
+    } else {
+        $(".answer-response").html("You answered <b>" + result + "</b>! The correct answer was " + answer);
+    }
+
+    $(".question-options").empty();
+
+    let answerImage = $("<img>");
+    answerImage.addClass("answerImg");
+    //add audio clip
     
+    let imageAddress = "assets/images/" + trivia.rounds[trivia.currentRound].answerImages[trivia.currentQuestion];
+
+    answerImage.attr("src", imageAddress);
+    $(".response-image").append(answerImage);
+
 }
--- assets/js/triviaObject.js
+++ assets/js/triviaObject.js
@@ -2,41 +2,49 @@ var trivia = {
     rounds: [
         //round 1
         { //classic rock
-            questions: ["1"],
-            //round answer options --> one around per question
-            options: [
-                {options: ["blondie", "led zepplin"]},
-                {options: ["blondie", "led zepplin"]},
-                {options: ["blondie", "led zepplin"]}
+            questions: ["What was one of its previous names before this new wave band settled on 'The Cars'?",
+             "2",
+              "3"
             ],
-            answers: []
+            //round answer options
+            options: [
+                {choices: ["Cap'n Swing", "Down n' Dirty", "The Cocktails", "Sound Garden"]},//question 1 choices
+                {choices: ["blondie", "led zepplin", "option3", "option4"]},//question 2 choices
+                {choices: ["blondie", "led zepplin", "option3", "option4"]}//question 3 choices
+            ],
+            answers: ["Cap'n Swing", "led zepplin", "option3"],
+            answerImages: ["capNSwing.jpg"]
+            
         },
         //round 2
         { //vanity
             questions: ["2"],
             //round answer options --> one around per question
             options: [
-                {options: ["blondie", "led zepplin"]},
-                {options: ["blondie", "led zepplin"]},
-                {options: ["blondie", "led zepplin"]}
+                {choices: ["blondie", "led zepplin", "option3", "option4"]},
+                {choices: ["blondie", "led zepplin", "option3", "option4"]},
+                {choices: ["blondie", "led zepplin", "option3", "option4"]}
             ],
-            answers: []
+            answers: [],
+            answerImages: ["capNSwing.jpg"]
         },
         //round 3
         { //politics
             questions: ["3"],
             //round answer options --> one around per question
             options: [
-                {options: ["blondie", "led zepplin"]},
-                {options: ["blondie", "led zepplin"]},
-                {options: ["blondie", "led zepplin"]}
+                {choices: ["blondie", "led zepplin", "option3", "option4"]},
+                {choices: ["blondie", "led zepplin", "option3", "option4"]},
+                {choices: ["blondie", "led zepplin", "option3", "option4"]}
             ],
-            answers: []
+            answers: [],
+            answerImages: ["capNSwing.jpg"]
         }
     ],
 
     currentRound: 0,
+    currentQuestion: 0,
     correctAnswers: 0,
     incorrectAnswers: 0,
-    timeRemaining: 0,
+    timeRemaining: 30,
 }
