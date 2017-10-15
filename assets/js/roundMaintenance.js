0e8da8d655f6e1fc7a995318627f5d9630cf3643
James Martineau <James.Martineau@libertymutual.com>
2017-10-15 15:05:14 -0400

    initial commit

---
+++ assets/js/roundMaintenance.js
@@ -0,0 +1,7 @@
+function showQuestion(){
+
+}
+
+function nextRound(){
+    
+}
---
+++ assets/js/triviaObject.js
@@ -0,0 +1,42 @@
+var trivia = {
+    rounds: [
+        //round 1
+        { //classic rock
+            questions: ["1"],
+            //round answer options --> one around per question
+            options: [
+                {options: ["blondie", "led zepplin"]},
+                {options: ["blondie", "led zepplin"]},
+                {options: ["blondie", "led zepplin"]}
+            ],
+            answers: []
+        },
+        //round 2
+        { //vanity
+            questions: ["2"],
+            //round answer options --> one around per question
+            options: [
+                {options: ["blondie", "led zepplin"]},
+                {options: ["blondie", "led zepplin"]},
+                {options: ["blondie", "led zepplin"]}
+            ],
+            answers: []
+        },
+        //round 3
+        { //politics
+            questions: ["3"],
+            //round answer options --> one around per question
+            options: [
+                {options: ["blondie", "led zepplin"]},
+                {options: ["blondie", "led zepplin"]},
+                {options: ["blondie", "led zepplin"]}
+            ],
+            answers: []
+        }
+    ],
+
+    currentRound: 0,
+    correctAnswers: 0,
+    incorrectAnswers: 0,
+    timeRemaining: 0,
+}
